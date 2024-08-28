4bb2109bbfd4d965c4bcdcda7b661815b1568b0a
gloriamatope <52277838+gloriamatope@users.noreply.github.com>
2024-08-28 23:20:44 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -3,10 +3,43 @@ function refreshweather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#weather-app-city");
-   
+    let humidityElement = document.querySelector("#humidity");
+    let descriptionElement = document.querySelector("#description");
+    let speedElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    
+    timeElement.innerHTML = formatDate(date);
+     speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
-     temperatureElement.innerHTML = Math.round(temperature);
+    temperatureElement.innerHTML = Math.round(temperature);
    
+
+}
+    function formatDate(date) {
+      
+        let minutes = date.getMinutes();
+        let hours = date.getHours();
+        let days = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
+        let day = days[date.getDay()];
+
+        if (minutes < 10) {
+        
+        minutes = `0${minutes}`;
+    }
+
+
+ return `${day} ${hours} ${minutes}`;  
 
 }
 
